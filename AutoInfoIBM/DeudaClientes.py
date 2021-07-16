9f74c23af64b17807bf318d3de76b479c15036d6
gpedro178 <gpedro178@gmail.com>
2021-07-16 14:23:19 -0300

    Connecting to DB

--- AutoInfoIBM/DeudaClientes.py
+++ AutoInfoIBM/DeudaClientes.py
@@ -1,18 +1,17 @@
 import pandas as pd
+from DatosLogin import login
 import pyodbc
-# Some other example server values are
-# server = "localhost\sqlexpress" # for a named instance
-# server = "myserver,port" # to specify an alternate port
-# server = "tcp:myserver.database.windows.net"
-server = "192.168.200.33,50020\cloud"
-database = "Rumaos"
-username = "gpedro" 
-password = "s3rv1d0"
+
+server = login[0]
+database = login[1]
+username = login[2] 
+password = login[3]
 try:
     cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};\
         SERVER="+server+";DATABASE="+database+";UID="+username+";PWD="+ password)
 except Exception as e:
     print("Ocurrió un error al conectar a SQL Server: ", e)
+    exit()
 
 cursor = cnxn.cursor()
 
