64869ca13fa91c7ec34e90a3b402e8c047d6ddac
gpedro178 <gpedro178@gmail.com>
2021-07-16 16:08:28 -0300

    Commenting Connection

--- AutoInfoIBM/DeudaClientes.py
+++ AutoInfoIBM/DeudaClientes.py
@@ -1,19 +1,24 @@
 import pandas as pd
-from DatosLogin import login
 import pyodbc
+
+# The "login" variable is a list that holds the connection data to the SQL Server
+from DatosLogin import login
 
 server = login[0]
 database = login[1]
 username = login[2] 
 password = login[3]
+
+# Try connecting to the SQL Server, will report error and stop if failed
+
 try:
-    cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};\
+    conex = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};\
         SERVER="+server+";DATABASE="+database+";UID="+username+";PWD="+ password)
 except Exception as e:
     print("Ocurrió un error al conectar a SQL Server: ", e)
     exit()
 
-cursor = cnxn.cursor()
+cursor = conex.cursor()
 
 #Sample Query
 cursor.execute("SELECT * FROM [Rumaos].[dbo].[Mail]")
