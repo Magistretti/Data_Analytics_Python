48730affbfbb7cd0a69ffaa4d79b2d411eeed5e7
gpedro178 <gpedro178@gmail.com>
2021-07-20 14:40:36 -0300

    Adding Faccli and excel Cliente Cond. Esp. as DF

--- AutoInfoIBM/DeudaClientes.py
+++ AutoInfoIBM/DeudaClientes.py
@@ -24,13 +24,53 @@ except Exception as e:
 
 cursor = db_conex.cursor()
 
+############
 #Sample Query
 # cursor.execute("SELECT * FROM [Rumaos].[dbo].[Mail]")
 # row = cursor.fetchone()
 # while row:
 #     print(row[1])
 #     row = cursor.fetchone()
+#
+# df_mail = pd.read_sql("SELECT * FROM [Rumaos].[dbo].[Mail]",db_conex)
+# print(df_mail)
+############
 
-df_mail = pd.read_sql("SELECT * FROM [Rumaos].[dbo].[Mail]",db_conex)
+df_cuentasDeudoras = pd.read_sql("""
+    SELECT 
+        FacCli.[ID] 
+        ,FacCli.[NROCLIPRO]
+        ,FacCli.[NOMBRE]
+        ,FacCli.[DOMICILIO]
+        ,FacCli.[LOCALIDAD]
+        ,FacCli.[PROVINCIA]
+        ,FacCli.[TELEFONO]
+        ,FacCli.[EMAIL]
+        ,FacCli.[TIPOIVA]
+        ,FacCli.[CUITDOC]
+        ,FacCli.[CODFORPAGO]
+        ,FacCli.[FEULTVTASQL]
+        ,FacCli.[SALDOPREPAGO]
+        ,FacCli.[SALDOREMIPENDFACTU]
+        ,FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU as SALDOTOTAL
+        ,FacCli.[TARJETA]
+        ,FacCli.[TIPOCOMPCC]
+        ,FacCli.[BLOQUEADO]
+        ,FacCli.[LIMITECREDITO]
+        ,Vendedores.[NOMBREVEND]
+        ,FacCli.[ListaSaldoCC]
+    FROM [Rumaos].[dbo].[FacCli]
+    left outer join Vendedores On FacCli.NROVEND = Vendedores.NROVEND
+    where ListaSaldoCC = 1
+        and FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU < -100;
+""", db_conex)
 
-print(df_mail)
+df_cuentasDeudoras.head(5)
+
+df_condicionCliente = pd.read_excel("C:/Users/gpedro/OneDrive - RedMercosur/"
+    "Consultas Power BI/TABLERO SGES VS SGFIN/Clientes condicion especial.xlsx",
+    usecols="B,C,I")
+
+df_condicionCliente.dropna(subset=["Condición del cliente"], inplace=True)
+
+print(df_condicionCliente)
