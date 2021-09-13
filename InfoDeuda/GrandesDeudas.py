76dc10ecde47079dca65d7155e3d3ae59f7e9eed
gpedro178 <gpedro178@gmail.com>
2021-09-13 16:47:46 -0300

    Start of InformeDeudas

---
+++ InfoDeuda/GrandesDeudas.py
@@ -0,0 +1,32 @@
+###################################
+#
+#     INFORME DEUDAS > $400.000,00
+#
+###################################
+
+import os
+from PIL import Image
+import pandas as pd
+import dataframe_image as dfi
+import pyodbc #Library to connect to Microsoft SQL Server
+from DatosLogin import login #Holds connection data to the SQL Server
+
+tiempoInicio = pd.to_datetime("today")
+
+#########
+# Formating display of dataframes with comma separator for numbers
+pd.options.display.float_format = "{:20,.0f}".format 
+#########
+
+# Try connecting to the SQL Server, will report error and stop if failed
+try:
+    db_conex = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};\
+        SERVER="+login[0]+";\
+        DATABASE="+login[1]+";\
+        UID="+login[2]+";\
+        PWD="+ login[3]
+    )
+except Exception as e:
+    print("Ocurrió un error al conectar a SQL Server: ", e)
+    exit()
