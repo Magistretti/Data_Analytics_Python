523f5eb2a2977cd8971d4c86f8b59095c5b62c74
gpedro178 <gpedro178@gmail.com>
2021-09-14 09:40:45 -0300

    Better error parsing

--- InfoDeuda/GrandesDeudas.py
+++ InfoDeuda/GrandesDeudas.py
@@ -25,8 +25,12 @@ try:
         SERVER="+login[0]+";\
         DATABASE="+login[1]+";\
         UID="+login[2]+";\
-        PWD="+ login[3]
+        PWD="+ login[2]
     )
 except Exception as e:
-    print("Ocurrió un error al conectar a SQL Server: ", e)
+    listaErrores = e.args[1].split(".")
+    print("\nOcurrió un error al conectar a SQL Server:")
+    for i in listaErrores:
+        print(i)
+    print("")
     exit()
