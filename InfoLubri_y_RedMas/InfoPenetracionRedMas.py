2c1acaf74bc45648160b60912af2b600b73675dd
gpedro178 <gpedro178@gmail.com>
2021-10-28 13:19:53 -0300

    Create and add row of columns totals

--- InfoLubri_y_RedMas/InfoPenetracionRedMas.py
+++ InfoLubri_y_RedMas/InfoPenetracionRedMas.py
@@ -59,12 +59,28 @@ df_desp_x_turno_liq = df_desp_x_turno_liq.convert_dtypes()
 df_desp_x_turno_liq["UEN"] = df_desp_x_turno_liq["UEN"].str.strip()
 df_desp_x_turno_liq["TURNO"] = df_desp_x_turno_liq["TURNO"].str.strip()
 
-# pivot_desp_totales_x_turno_liq = pd.pivot_table(df_desp_x_turno_liq
-#     , values=["Despachos RedMas","Despachos"]
-#     , columns="TURNO"
-#     , aggfunc="sum"
-# )
-# print(pivot_desp_totales_x_turno_liq.head())
+# Create a Pivot Table of "Despachos RedMas" and "Despachos" grouped by "TURNO"
+pivot_desp_totales_x_turno_liq = pd.pivot_table(df_desp_x_turno_liq
+    , values=["Despachos RedMas","Despachos"]
+    , columns="TURNO"
+    , aggfunc="sum"
+)
+# Create column "TOTAL" with the total of each row
+pivot_desp_totales_x_turno_liq = \
+    pivot_desp_totales_x_turno_liq.assign(TOTAL= lambda row: row.sum(1))
+# Create a row with the values of "Despachos RedMas" divided by "Despachos"
+pivot_desp_totales_x_turno_liq = \
+    pivot_desp_totales_x_turno_liq.append(
+        pivot_desp_totales_x_turno_liq.loc["Despachos RedMas"] /
+        pivot_desp_totales_x_turno_liq.loc["Despachos"]
+        , ignore_index=True
+    )
+# Get the row with the results of the last calculation 
+total_penet_x_turno_liq = pivot_desp_totales_x_turno_liq.loc[[2]]
+# Add column UEN with value = "TOTAL"
+total_penet_x_turno_liq.insert(0,"UEN",["TOTAL"])
+# Rename index as "colTotal"
+total_penet_x_turno_liq.rename({2:"colTotal"}, inplace=True)
 
 #Get "PENETRACIÓN" by "UEN" and "TURNO"
 df_desp_x_turno_liq["PENETRACIÓN"] = df_desp_x_turno_liq.apply(
@@ -89,13 +105,14 @@ pivot_desp_liq_total = pd.pivot_table(df_desp_x_turno_liq
     , fill_value=0
 )
 pivot_desp_liq_total.reset_index(inplace=True)
+# Create the column "TOTAL" with the totals per row
 pivot_desp_liq_total["TOTAL"] = pivot_desp_liq_total.apply(
     lambda row: (row["Despachos RedMas"])/(row["Despachos"])
     , axis= 1
 )
 pivot_desp_liq_total = pivot_desp_liq_total[["UEN","TOTAL"]]
 
-
+# Merge the column "TOTAL" to the pivot_desp_x_turno_liq
 df_penetRM_liq_x_turno = pd.merge(
     pivot_desp_x_turno_liq,
     pivot_desp_liq_total,
@@ -103,6 +120,13 @@ df_penetRM_liq_x_turno = pd.merge(
     how="inner"
 )
 df_penetRM_liq_x_turno.sort_values(by=["TOTAL"],inplace=True)
+
+# Add the row with the totals per column
+df_penetRM_liq_x_turno = pd.concat(
+    [df_penetRM_liq_x_turno
+        , total_penet_x_turno_liq
+    ]
+)
 
 print("REDMAS PENETRACION LIQ")
 print(df_penetRM_liq_x_turno)
@@ -141,8 +165,30 @@ df_desp_x_turno_GNC = df_desp_x_turno_GNC.convert_dtypes()
 df_desp_x_turno_GNC["UEN"] = df_desp_x_turno_GNC["UEN"].str.strip()
 df_desp_x_turno_GNC["TURNO"] = df_desp_x_turno_GNC["TURNO"].str.strip()
 
+# Create a Pivot Table of "Despachos RedMas" and "Despachos" grouped by "TURNO"
+pivot_desp_totales_x_turno_GNC = pd.pivot_table(df_desp_x_turno_GNC
+    , values=["Despachos RedMas","Despachos"]
+    , columns="TURNO"
+    , aggfunc="sum"
+)
+# Create column "TOTAL" with the total of each row
+pivot_desp_totales_x_turno_GNC = \
+    pivot_desp_totales_x_turno_GNC.assign(TOTAL= lambda row: row.sum(1))
+# Create a row with the values of "Despachos RedMas" divided by "Despachos"
+pivot_desp_totales_x_turno_GNC = \
+    pivot_desp_totales_x_turno_GNC.append(
+        pivot_desp_totales_x_turno_GNC.loc["Despachos RedMas"] /
+        pivot_desp_totales_x_turno_GNC.loc["Despachos"]
+        , ignore_index=True
+    )
+# Get the row with the results of the last calculation 
+total_penet_x_turno_GNC = pivot_desp_totales_x_turno_GNC.loc[[2]]
+# Add column UEN with value = "TOTAL"
+total_penet_x_turno_GNC.insert(0,"UEN",["TOTAL"])
+# Rename index as "colTotal"
+total_penet_x_turno_GNC.rename({2:"colTotal"}, inplace=True)
 
-#Get "PENETRACIÓN" by "UEN" and "TURNO"
+# Get "PENETRACIÓN" by "UEN" and "TURNO"
 df_desp_x_turno_GNC["PENETRACIÓN"] = df_desp_x_turno_GNC.apply(
     lambda row: (row["Despachos RedMas"])/(row["Despachos"])
     , axis= 1
@@ -165,13 +211,14 @@ pivot_desp_GNC_total = pd.pivot_table(df_desp_x_turno_GNC
     , fill_value=0
 )
 pivot_desp_GNC_total.reset_index(inplace=True)
+# Create the column "TOTAL" with the totals per row
 pivot_desp_GNC_total["TOTAL"] = pivot_desp_GNC_total.apply(
     lambda row: (row["Despachos RedMas"])/(row["Despachos"])
     , axis= 1
 )
 pivot_desp_GNC_total = pivot_desp_GNC_total[["UEN","TOTAL"]]
 
-
+# Merge the column "TOTAL" to the pivot_desp_x_turno_GNC
 df_penetRM_GNC_x_turno = pd.merge(
     pivot_desp_x_turno_GNC,
     pivot_desp_GNC_total,
@@ -180,5 +227,12 @@ df_penetRM_GNC_x_turno = pd.merge(
 )
 df_penetRM_GNC_x_turno.sort_values(by=["TOTAL"],inplace=True)
 
-print("REDMAS PENETRACION GNC")
+# Add the row with the totals per column
+df_penetRM_GNC_x_turno = pd.concat(
+    [df_penetRM_GNC_x_turno
+        , total_penet_x_turno_GNC
+    ]
+)
+
+print("\nREDMAS PENETRACION GNC")
 print(df_penetRM_GNC_x_turno)
