3d9489f98878a36c279b03b62d9aff99e08773f5
gpedro178 <gpedro178@gmail.com>
2021-11-11 16:50:18 -0300

    month query to df

--- InfoSemanal/InfoRedControl.py
+++ InfoSemanal/InfoRedControl.py
@@ -39,6 +39,11 @@ df_nrocli_omit = pd.read_excel(
 )
 df_codprod_ok = df_codprod_ok.convert_dtypes()
 
+
+##########################################
+# Function to help filter dataframes
+##########################################
+
 def _filtrador(df, tipo_cuenta="CC"):
     '''
     df: Dataframe to be filtered
@@ -81,6 +86,7 @@ def _filtrador(df, tipo_cuenta="CC"):
         raise ValueError("Arg 'tipo_cuenta' must be 'CC' or 'CA'")
     
     return df
+
 
 ##########################################
 # Get "remitos" of previous week
@@ -140,6 +146,7 @@ df_rem_sem_ant = pd.read_sql(
         ,conexMSSQL)
 df_rem_sem_ant = df_rem_sem_ant.convert_dtypes()
 
+
 ##########################################
 # Get "remitos" of current week
 ##########################################
@@ -195,3 +202,121 @@ df_rem_sem_act = pd.read_sql(
 df_rem_sem_act = df_rem_sem_act.convert_dtypes()
 
 
+##########################################
+# Get "remitos" of previous month
+##########################################
+
+df_rem_sem_act = pd.read_sql(
+    """
+    DECLARE @inicioMesActual DATETIME
+    SET @inicioMesActual = DATEADD(
+        month
+        , DATEDIFF(month, 0, CURRENT_TIMESTAMP)
+        , 0
+    )
+
+    DECLARE @inicioMesAnterior DATETIME
+    SET @inicioMesAnterior = DATEADD(M,-1,@inicioMesActual)
+
+    --Divide por la cant de días del mes anterior y multiplica
+    --por la cant de días del mes actual
+    DECLARE @pondMesAnt FLOAT
+    SET @pondMesAnt = CAST(DAY(EOMONTH(CURRENT_TIMESTAMP)) AS float) /
+        CAST(DAY(EOMONTH(@inicioMesAnterior)) AS float)
+
+    SELECT 
+        RTRIM(FRD.[UEN]) AS 'UEN'
+        ,RTRIM(FRD.[CODPRODUCTO]) AS 'CODPRODUCTO'
+        ,FRD.[NROCLIENTE]
+        ,(FRD.[CANTIDAD] * @pondMesAnt) AS 'Mes Anterior'
+        ,(FC.SALDOPREPAGO - FC.SALDOREMIPENDFACTU) as 'Saldo_Cta'
+    FROM [Rumaos].[dbo].[FacRemDet] as FRD
+    JOIN Rumaos.dbo.FacCli as FC
+        on FRD.NROCLIENTE = FC.NROCLIPRO
+    WHERE FRD.FECHASQL >= @inicioMesAnterior
+        AND FRD.FECHASQL < @inicioMesActual
+        AND FRD.NROCLIENTE >= '100000'
+        AND FRD.CANTIDAD > 0
+        AND FRD.UEN IN (
+            'MERC GUAYMALLEN'
+            ,'MERCADO 2'
+            ,'MITRE'
+            ,'PERDRIEL'
+            ,'PERDRIEL2'
+            ,'PUENTE OLIVE'
+        )
+        AND FRD.CODPRODUCTO NOT IN (
+            'GNC'
+            ,'GNC01'
+            ,'GNC02'
+            ,'GNC03'
+            ,'GNCA'
+            ,'GNCCA'
+            ,'GNCCC'
+            ,'GNCCO'
+            ,'GNCRM'
+            ,'GNCVA'
+            ,'GNNC1'
+        )
+    """
+        ,conexMSSQL)
+
+
+##########################################
+# Get "remitos" of current month
+##########################################
+
+df_rem_sem_act = pd.read_sql(
+    """
+    DECLARE @inicioMesActual DATETIME
+    SET @inicioMesActual = DATEADD(
+        month
+        , DATEDIFF(month, 0, CURRENT_TIMESTAMP)
+        , 0
+    )
+
+    DECLARE @hoy DATETIME
+    SET @hoy = DATEADD(DAY, DATEDIFF(DAY, 0, CURRENT_TIMESTAMP), 0)
+
+    --Divide por la cantidad de días cursados del mes actual y multiplica 
+    --por la cant de días del mes actual
+    DECLARE @pondMesAct FLOAT
+    SET @pondMesAct = CAST(DAY(EOMONTH(CURRENT_TIMESTAMP)) AS float) /
+        (CAST(DAY(CURRENT_TIMESTAMP) AS float)-1)
+
+    SELECT 
+        RTRIM(FRD.[UEN]) AS 'UEN'
+        ,RTRIM(FRD.[CODPRODUCTO]) AS 'CODPRODUCTO'
+        ,FRD.[NROCLIENTE]
+        ,(FRD.[CANTIDAD] * @pondMesAct) AS 'Mes Actual'
+        ,(FC.SALDOPREPAGO - FC.SALDOREMIPENDFACTU) as 'Saldo_Cta'
+    FROM [Rumaos].[dbo].[FacRemDet] as FRD
+    JOIN Rumaos.dbo.FacCli as FC
+        on FRD.NROCLIENTE = FC.NROCLIPRO
+    WHERE FRD.FECHASQL >= @inicioMesActual
+        AND FRD.FECHASQL < @hoy
+        AND FRD.NROCLIENTE >= '100000'
+        AND FRD.CANTIDAD > 0
+        AND FRD.UEN IN (
+            'MERC GUAYMALLEN'
+            ,'MERCADO 2'
+            ,'MITRE'
+            ,'PERDRIEL'
+            ,'PERDRIEL2'
+            ,'PUENTE OLIVE'
+        )
+        AND FRD.CODPRODUCTO NOT IN (
+            'GNC'
+            ,'GNC01'
+            ,'GNC02'
+            ,'GNC03'
+            ,'GNCA'
+            ,'GNCCA'
+            ,'GNCCC'
+            ,'GNCCO'
+            ,'GNCRM'
+            ,'GNCVA'
+            ,'GNNC1'
+        )
+    """
+        ,conexMSSQL)
