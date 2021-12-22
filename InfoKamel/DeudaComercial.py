054f4016bae13da8cda5225f91bdff413ce7bb36
gpedro178 <gpedro178@gmail.com>
2021-12-22 12:32:12 -0300

    Code update, debugging merge

--- InfoKamel/DeudaComercial.py
+++ InfoKamel/DeudaComercial.py
@@ -5,11 +5,17 @@
 ###################################
 
 import os
+import sys
+import pathlib
+
+# Allow imports from the top folder
+sys.path.insert(0,str(pathlib.Path(__file__).parent.parent))
+
 import pandas as pd
-#########
-# Formating display of dataframes with comma separator for numbers
-pd.options.display.float_format = "{:20,.2f}".format 
-#########
+import dataframe_image as dfi
+
+from DatosLogin import login
+from Conectores import conectorMSSQL
 
 import logging
 logging.basicConfig(
@@ -18,74 +24,16 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-import dataframe_image as dfi
+
+#########
+# Formating display of dataframes with comma separator for numbers
+pd.options.display.float_format = "{:20,.2f}".format 
+#########
 
 tiempoInicio = pd.to_datetime("today")
 
-def biggestOf2(a,b):
-    if a > b:
-        return a
-    else:
-        return b
 
-# We are connecting to a Microsoft SQL Server so we can use pyodbc library
-import pyodbc
-
-# "login" is a list that holds the connection data to the SQL Server
-from DatosLogin import login
-
-server = login[0]
-database = login[1]
-username = login[2] 
-password = login[3]
-
-# Try connecting to the SQL Server, will report error and stop if failed
-try:
-    db_conex = pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};\
-        SERVER="+server+";\
-        DATABASE="+database+";\
-        UID="+username+";\
-        PWD="+ password
-    )
-except Exception as e:
-    listaErrores = e.args[1].split(".")
-    logger.error("\nOcurrió un error al conectar a SQL Server: ")
-    for i in listaErrores:
-        logger.error(i)
-    exit()
-
-
-#########
-# Table "Clientes condicion especial.xlsx" NOT NEEDED ANYMORE
-#
-# Convert Excel file "Clientes condicion especial.xlsx" to a Dataframe
-#
-# Requirements:
-# -Filter unused columns
-# -Drop rows with NaNs in "Condición del cliente" column
-#########
-
-# df_condicionCliente = pd.read_excel("C:/Users/gpedro/OneDrive - RedMercosur/"
-#     "Consultas Power BI/TABLERO SGES VS SGFIN/"
-#     "Clientes condicion especial.xlsx",
-#     usecols="B,I")
-
-# df_condicionCliente["NROCLIPRO"] = \
-#     df_condicionCliente["NROCLIPRO"].astype("string")
-
-# df_condicionCliente.dropna(subset=["Condición del cliente"], inplace=True)
-
-#print(df_condicionCliente)
-
-#########
-# Left Outer Merge of df_cuentasDeudoras with df_condicionCliente to add 
-# "Condición del cliente" column to df_cuentasDeudoras
-#########
-
-# df_cuentasDeudoras = df_cuentasDeudoras.merge(right=df_condicionCliente,
-#     how="left", on="NROCLIPRO")
-# df_cuentasDeudoras.info()
+conexMSSQL = conectorMSSQL(login)
 
 
 #########
@@ -103,20 +51,19 @@ except Exception as e:
 #   to get only debt below -100
 ######### 
 
-df_cuentasDeudoras = pd.read_sql("""
-    SELECT 
-        CAST(FacCli.[NROCLIPRO] AS VARCHAR) as NROCLIENTE
-        ,FacCli.[NOMBRE]
-        ,FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU as SALDOCUENTA
-    FROM [Rumaos].[dbo].[FacCli] WITH (NOLOCK)
-    where ListaSaldoCC = 1
-        and FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU < -100;
-""", db_conex)
+df_cuentasDeudoras = pd.read_sql(
+    """
+        SELECT 
+            CAST(FacCli.[NROCLIPRO] AS VARCHAR) as NROCLIENTE
+            ,FacCli.[NOMBRE]
+            ,FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU as SALDOCUENTA
+        FROM [Rumaos].[dbo].[FacCli] WITH (NOLOCK)
+        where ListaSaldoCC = 1
+            and (FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU) < -1000;
+    """, conexMSSQL
+)
 
 df_cuentasDeudoras = df_cuentasDeudoras.convert_dtypes()
-
-#print(df_cuentasDeudoras.info())
-#print(df_cuentasDeudoras.head(5))
 
 
 #########
@@ -131,23 +78,23 @@ df_cuentasDeudoras = df_cuentasDeudoras.convert_dtypes()
 fechaAñoAtras = (tiempoInicio - pd.Timedelta(365, unit="d"))\
     .strftime("'%Y%m%d'")
 
-df_remitos = pd.read_sql("""
-    SELECT 
-        FacRemDet.[FECHASQL]
-        ,cast(FacRemDet.[NROCLIENTE] AS VARCHAR) as NROCLIENTE
-        ,FacCli.[NOMBRE]
-        ,FacRemDet.[IMPORTE]
-    FROM [Rumaos].[dbo].[FacRemDet] WITH (NOLOCK)
-    Left Outer Join FacCli WITH (NOLOCK) 
-        on FacRemDet.NROCLIENTE = FacCli.NROCLIPRO
-    where FECHASQL >= """+fechaAñoAtras+"""
-        and ListaSaldoCC = 1
-        and FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU < -100
-""", db_conex)
+df_remitos = pd.read_sql(
+    """
+        SELECT 
+            FacRemDet.[FECHASQL]
+            ,cast(FacRemDet.[NROCLIENTE] AS VARCHAR) as NROCLIENTE
+            ,FacCli.[NOMBRE]
+            ,FacRemDet.[IMPORTE]
+        FROM [Rumaos].[dbo].[FacRemDet] WITH (NOLOCK)
+        Left Outer Join FacCli WITH (NOLOCK) 
+            on FacRemDet.NROCLIENTE = FacCli.NROCLIPRO
+        where FECHASQL >= """+fechaAñoAtras+"""
+            and ListaSaldoCC = 1
+            and (FacCli.SALDOPREPAGO - FacCli.SALDOREMIPENDFACTU) < -1000
+    """, conexMSSQL
+)
 
-#print(df_remitos.head(5))
 df_remitos = df_remitos.convert_dtypes()
-#df_remitos.info()
 
 
 ############
@@ -171,7 +118,7 @@ df_ventaPesosPorCuenta = df_remitos[["NROCLIENTE","NOMBRE","IMPORTE"]]\
 
 #print(df_ventaPesosPorCuenta.head())
 
-fechaHoy = pd.to_datetime("today").normalize()
+fechaHoy = pd.to_datetime("today").normalize() #datetime at 00:00:00
 
 fechaSemanaAtras = fechaHoy - pd.to_timedelta(7, unit="days")
 
@@ -210,10 +157,19 @@ df_remitosVentasPorCliente = pd.merge(
 df_remitosVentasPorCliente = pd.merge(
     df_remitosVentasPorCliente,
     df_VentaSemanalPorCuenta,
+    how="left",
     on=["NROCLIENTE","NOMBRE"],
     suffixes=("","_Semanal")
 )
-
+df_remitosVentasPorCliente.fillna(0, inplace=True)
+df_remitosVentasPorCliente.reset_index(inplace=True)
+print(df_remitosVentasPorCliente[df_remitosVentasPorCliente["NROCLIENTE"]=="101228"])
+############################################################
+############################################################
+############################################################
+############################################################
+############################################################
+raise SystemExit()
 #######
 # Creating columns: 
 #   -"Dias Entre 1er y Ultimo Remito" 
@@ -224,7 +180,8 @@ df_remitosVentasPorCliente = pd.merge(
 
 df_remitosVentasPorCliente["Dias Entre 1er y Ultimo Remito"] = \
     df_remitosVentasPorCliente.apply(
-        lambda row: (row["FECHASQL_UltimoRemito"]-row["FECHASQL_PrimerRemito"])
+        lambda row: 
+            (row["FECHASQL_UltimoRemito"]-row["FECHASQL_PrimerRemito"])
             if (row["FECHASQL_UltimoRemito"]-row["FECHASQL_PrimerRemito"]) >
                 pd.to_timedelta(0, unit="days") #Compare timedelta > 0
             else pd.to_timedelta(1, unit="days") #To avoid divide by 0
@@ -262,6 +219,14 @@ df_remitosVentasPorCliente = pd.merge(
     on=["NROCLIENTE","NOMBRE"]
 )
 
+
+def biggestOf2(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
 df_remitosVentasPorCliente["Dias Venta Adeud"] = \
     df_remitosVentasPorCliente.apply(
         lambda row: round(row["SALDOCUENTA"] * (-1) / 
@@ -280,10 +245,17 @@ def condDeuda(diasAdeudados):
         return "Moroso"
     else:
         return "Excedido"
-
+######################################################
+######################################################
+######################################################
+######################################################
+######################################################
+# TODO: Apply "INCOBRABLE" tag to certain "NROCLIENTE"
 df_remitosVentasPorCliente["Cond Deuda Cliente"] = \
     df_remitosVentasPorCliente.apply(
-        lambda row: condDeuda(row["Dias Venta Adeud"])
+        lambda row: "INCOBRABLE"
+            if row["NROCLIENTE"] == "101228"
+            else condDeuda(row["Dias Venta Adeud"])
         , axis= 1
     )
 
@@ -381,7 +353,7 @@ except:
 # PRINTING dataframe as an image
 ##############
 
-ubicacion = "C:\Informes\InfoMorosos\\"
+ubicacion = str(pathlib.Path(__file__).parent)+"\\"
 
 # This will print the df with name and time so you can have multiple
 # files in the same folder
@@ -397,13 +369,13 @@ ubicacion = "C:\Informes\InfoMorosos\\"
 # This will print the df with a unique name and will erase the old image 
 # everytime the script is run
 
-if os.path.exists(ubicacion+"Info_Morosos.png"):
-    os.remove(ubicacion+"Info_Morosos.png")
+if os.path.exists(ubicacion+"test.png"):
+    os.remove(ubicacion+"test.png")
     dfi.export(df_conEstilo_condCtaRetrasadas, 
-        ubicacion+"Info_Morosos.png")
+        ubicacion+"test.png")
 else:
     dfi.export(df_conEstilo_condCtaRetrasadas, 
-        ubicacion+"Info_Morosos.png")
+        ubicacion+"test.png")
 
 
 # Timer
