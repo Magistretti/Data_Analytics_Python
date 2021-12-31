167bb8866fc1af8fa729943fff9139230f2a521f
gpedro178 <gpedro178@gmail.com>
2021-12-31 11:27:44 -0300

    Updated ChequesSaldosSGFin with Echeqs

--- InfoKamel/ChequesSaldosSGFin.py
+++ InfoKamel/ChequesSaldosSGFin.py
@@ -13,8 +13,10 @@ sys.path.insert(0,str(pathlib.Path(__file__).parent.parent))
 
 import pandas as pd
 import dataframe_image as dfi
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
-from DatosLogin import loginSGFin
+from DatosLogin import loginSGFin, googleSheet_InfoKamel
 from Conectores import conectorMSSQL
 
 import logging
@@ -26,7 +28,16 @@ logger = logging.getLogger(__name__)
 
 
 
+####################################################################
+# Get DF function
+####################################################################
+
 def _get_df(conexMSSQL):
+
+    ####################################################################
+    # Get state of the checks wallet in a df from a SQL query
+    ####################################################################
+
     df_cheques = pd.read_sql(
         """
         -- SALDO INICIAL, ENTRADAS, SALIDAS Y SALDO FINAL DE CHEQUES EN CARTERA
@@ -127,6 +138,93 @@ def _get_df(conexMSSQL):
     return df_cheques
 
 
+
+####################################################################
+# Get ECheq stock into a DF from a Google Sheet
+####################################################################
+
+def _get_df_GSheet(spreadsheetID, range):
+    """
+    Will read the selected range of a sheet from GoogleSheet and will return
+    a dataframe. NOTE: dates will be imported as formatted strings and should
+    be transformed accordingly.
+    ARGS: \\
+    spreadsheetID: can be obtained from the share link. Example: 
+    https://docs.google.com/spreadsheets/d/<SpreadSheetID>/edit?usp=sharing \\
+    range: range of a sheet to read in A1 notation. Example: "Dólar!A:E"
+    """
+
+    # Scopes will limit what we can do with the sheet
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'] # Read Only
+    SERVICE_ACCOUNT_FILE = \
+        str(pathlib.Path(__file__).parent.parent) + "\\quickstart.json"
+
+    # Credentials and service for the Sheets API
+    creds = service_account.Credentials.from_service_account_file(
+                SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    service = build('sheets', 'v4', credentials=creds, cache_discovery=False)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+
+
+    requestEcheq = sheet.values().get(
+        spreadsheetId=spreadsheetID # Spreadsheet ID
+        , range=range
+            # # valueRenderOption default to "FORMATTED_VALUE", it get strings
+        , valueRenderOption="UNFORMATTED_VALUE" # Will get numbers like numbers
+            # # dateTimeRenderOption default to "SERIAL_NUMBER" unless 
+            # # valueRenderOption is "FORMATTED_VALUE"
+        , dateTimeRenderOption="FORMATTED_STRING" # Will get dates as string
+    )
+
+    # Run the request
+    responseEcheq = requestEcheq.execute()
+
+    # Get the values of the sheet from the Json. This will be a list of lists
+    responseEcheq = responseEcheq.get("values")
+    
+    # Transform response into a DF, use the first row has header
+    df_stockEcheq = pd.DataFrame(
+        responseEcheq[1:] # Row values
+        , columns=responseEcheq[0] # Headers
+    )
+
+    # Cast string of dates as datetime
+    df_stockEcheq["Fecha"] = pd.to_datetime(
+        df_stockEcheq["Fecha"]
+        , dayfirst=True # Specify that strings are in the ddmmyyyy format
+    )
+
+    df_stockEcheq = df_stockEcheq.convert_dtypes()
+    
+    # Get stock of today, today date is normalized to reset time part of date
+    checkStock = df_stockEcheq[
+        df_stockEcheq["Fecha"] == pd.to_datetime("today").normalize()
+    ].copy() # .copy() will avoid raising "SettingWithCopyWarning"
+
+    # Fill NaN with zero in case of missing data
+    checkStock.fillna({"Saldo Final": 0}, inplace=True)
+
+    # If we have data today, use today data and remove date column
+    if len(checkStock.index) > 0:
+        df_stockEcheq = checkStock
+        df_stockEcheq = df_stockEcheq.drop(columns=["Fecha"])
+
+    # If we dont have data today, get a DF with zeroes
+    elif len(checkStock.index) == 0:
+        df_zeroValues = pd.DataFrame({
+            "UEN": "ECheq"
+            , "Saldo Final": 0
+        })
+        df_stockEcheq = df_zeroValues
+        
+
+
+    return df_stockEcheq
+
+
 ##########################################
 # STYLING of the dataframe
 ##########################################
@@ -178,15 +276,15 @@ ARGS:
                 ]
             }
         ]) \
-        # .apply(lambda x: ["background-color: black" if x.name == df.index[-1] 
-        #     else "" for i in x]
-        #     , axis=1) \
-        # .apply(lambda x: ["color: white" if x.name == df.index[-1]
-        #     else "" for i in x]
-        #     , axis=1) \
-        # .apply(lambda x: ["font-size: 15px" if x.name == df.index[-1]
-        #     else "" for i in x]
-        #     , axis=1)
+        .apply(lambda x: ["background-color: black" if x.name == df.index[-1] 
+            else "" for i in x]
+            , axis=1) \
+        .apply(lambda x: ["color: white" if x.name == df.index[-1]
+            else "" for i in x]
+            , axis=1) \
+        .apply(lambda x: ["font-size: 15px" if x.name == df.index[-1]
+            else "" for i in x]
+            , axis=1)
 
     return resultado
 
@@ -238,10 +336,23 @@ def chequesSaldos():
 
     # Get DF
     df_cheques = _get_df(conexMSSQL)
+    df_Echeq = _get_df_GSheet(googleSheet_InfoKamel, "ECheq!A:C")
+
+    # Append DFs
+    df_chequesConEcheq = df_cheques.append(df_Echeq)
+    # Fill the NaNs
+    df_chequesConEcheq.fillna(0, inplace=True)
+
+    # Get "TOTAL" row
+    df_chequesConEcheq.loc[df_chequesConEcheq.index[-1]+1] = \
+        df_chequesConEcheq.sum(numeric_only=True)
+
+    # Renaming NA in "UEN" column to "TOTAL"
+    df_chequesConEcheq.fillna({"UEN": "TOTAL"}, inplace=True)
 
     # Styling of DF
-    df_cheques_Estilo = _estiladorVtaTitulo(
-        df_cheques
+    df_chequesConEcheq_Estilo = _estiladorVtaTitulo(
+        df_chequesConEcheq
         , [
             "Saldo Inicial"
             , "Ingreso Cheq"
@@ -254,7 +365,7 @@ def chequesSaldos():
     ubicacion = str(pathlib.Path(__file__).parent)+"\\"
 
     # Get image of df_arqueos_Estilo
-    _df_to_image(df_cheques_Estilo, ubicacion, "ChequesSaldos.png")
+    _df_to_image(df_chequesConEcheq_Estilo, ubicacion, "ChequesSaldos.png")
 
 
     # Timer
