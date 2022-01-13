69c48d030494b66a3c76ab0ff7d0261cfcb4c690
gpedro178 <gpedro178@gmail.com>
2022-01-13 17:27:03 -0300

    Add merge of images to create final report

--- InfoSemanal/InfoPerifericos.py
+++ InfoSemanal/InfoPerifericos.py
@@ -1115,3 +1115,490 @@ def _get_lubriplaya(conexCentral):
 ##########################################################
 # Get dataframes of "Grill" sales
 ##########################################################
+
+def _get_grill(conexCentral):
+    """
+    Sales of Grill of actual and previous week
+    """
+
+    df_grill = pd.read_sql(
+        """
+        -- Importe total y unidades vendidas de Barrica Grill para un período determinado
+
+
+        DECLARE @ayer date
+        set @ayer = GETDATE()-1
+        DECLARE @semanaAtras date
+        set @semanaAtras = GETDATE()-8
+        DECLARE @dosSemanasAtras date
+        set @dosSemanasAtras = GETDATE()-15;
+
+
+        SELECT
+            GrillActual.UEN
+            ,GrillAnterior.[Unid Vend Sem Ant]
+            ,GrillActual.[Unid Vend Sem Actual]
+            ,GrillAnterior.[Importe Total Sem Ant]
+            ,GrillActual.[Importe Total Sem Actual]
+
+            
+        FROM(
+            SELECT
+                t.UEN
+                ,sum(t.[unid vendidas]) as 'Unid Vend Sem Actual'
+                ,sum(t.[Importe Total]) as 'Importe Total Sem Actual'
+
+            FROM(
+                SELECT
+                    BAG.UEN
+                    , subBAG.[unid vendidas]
+                    , BAG.[Importe Total]
+
+                FROM (-- Subquery para sumar importes por UEN
+                    SELECT 
+                        RTRIM(BAG.[UEN]) as 'UEN'
+                        ,sum(BAG.IMPORTE) as 'Importe Total'
+                    FROM [Rumaos].[dbo].[BAGrillDet] as BAG WITH (NOLOCK)
+                    WHERE CAST(BAG.FECHASQL as date) > @semanaAtras
+                        AND CAST(BAG.FECHASQL as date) <= @ayer
+                    group by BAG.UEN
+                ) as BAG
+
+                LEFT JOIN (-- Subquery para evitar cód '9999' durante la suma de cantidades
+                    SELECT 
+                        RTRIM(subBAG.[UEN]) as 'UEN'
+                        ,sum(subBAG.[CANTIDAD]) as 'unid vendidas'
+                    FROM [Rumaos].[dbo].[BAGrillDet] as subBAG WITH (NOLOCK)
+                    WHERE CAST(subBAG.FECHASQL as date) > @semanaAtras
+                        AND CAST(subBAG.FECHASQL as date) <= @ayer
+                        and subBAG.CODPRODUCTO <> '9999'
+                    group by subBAG.UEN
+                ) as subBAG
+            
+                    ON BAG.UEN = subBAG.UEN
+
+                UNION ALL -- Para concatenar datos de la [VIEW_VENTAS_POR_EMPLEADO]
+
+                (SELECT
+                    RTRIM([UEN]) as 'UEN'
+                    ,sum([CANTIDAD]) as 'unid vend'
+                    ,sum([IMPORTE]) as 'Importe Total'
+                FROM [Rumaos].[dbo].[VIEW_VENTAS_POR_EMPLEADO] WITH (NOLOCK)
+                where CODIGO not in (
+                    'MED'
+                    ,'TOR'
+                    ,'DOCML'
+                    ,'DOCTO'
+                    ,'MEDML'
+                    ,'MEDTO'
+                    ,'DOCMIX'
+                    ,'DOCMR'
+                    ,'MEDMIX'
+                    ,'MEDMR'
+                    ,'MEDRE'
+                    ,'MIX'
+                )
+                    AND AGRUPACION = 'PROMOS'
+                    AND UEN = 'PERDRIEL'
+                    AND CAST(FECHASQL as date) > @semanaAtras
+                    AND CAST(FECHASQL as date) <= @ayer
+
+                GROUP BY UEN)
+            ) as t
+
+        group by t.UEN
+        ) as GrillActual
+
+        JOIN (
+            SELECT
+                GrillAnterior.UEN
+                ,GrillAnterior.[Unid Vend Sem Ant]
+                ,GrillAnterior.[Importe Total Sem Ant]
+            FROM(
+                SELECT
+                    t.UEN
+                    ,sum(t.[unid vendidas]) as 'Unid Vend Sem Ant'
+                    ,sum(t.[Importe Total]) as 'Importe Total Sem Ant'
+
+                FROM(
+                    SELECT
+                        BAG.UEN
+                        , subBAG.[unid vendidas]
+                        , BAG.[Importe Total]
+
+                    FROM (-- Subquery para sumar importes por UEN
+                        SELECT 
+                            RTRIM(BAG.[UEN]) as 'UEN'
+                            ,sum(BAG.IMPORTE) as 'Importe Total'
+                        FROM [Rumaos].[dbo].[BAGrillDet] as BAG WITH (NOLOCK)
+                        WHERE CAST(BAG.FECHASQL as date) > @dosSemanasAtras
+                        AND CAST(BAG.FECHASQL as date) <= @semanaAtras
+                        group by BAG.UEN
+                    ) as BAG
+
+                    LEFT JOIN (-- Subquery para evitar cód '9999' durante la suma de cantidades
+                        SELECT 
+                            RTRIM(subBAG.[UEN]) as 'UEN'
+                            ,sum(subBAG.[CANTIDAD]) as 'unid vendidas'
+                        FROM [Rumaos].[dbo].[BAGrillDet] as subBAG WITH (NOLOCK)
+                        WHERE CAST(subBAG.FECHASQL as date) > @dosSemanasAtras
+                        AND CAST(subBAG.FECHASQL as date) <= @semanaAtras
+                            and subBAG.CODPRODUCTO <> '9999'
+                        group by subBAG.UEN
+                    ) as subBAG
+            
+                        ON BAG.UEN = subBAG.UEN
+
+                    UNION ALL -- Para concatenar datos de la [VIEW_VENTAS_POR_EMPLEADO]
+
+                    (SELECT
+                        RTRIM([UEN]) as 'UEN'
+                        ,sum([CANTIDAD]) as 'unid vend'
+                        ,sum([IMPORTE]) as 'Importe Total'
+                    FROM [Rumaos].[dbo].[VIEW_VENTAS_POR_EMPLEADO] WITH (NOLOCK)
+                    where CODIGO not in (
+                        'MED'
+                        ,'TOR'
+                        ,'DOCML'
+                        ,'DOCTO'
+                        ,'MEDML'
+                        ,'MEDTO'
+                        ,'DOCMIX'
+                        ,'DOCMR'
+                        ,'MEDMIX'
+                        ,'MEDMR'
+                        ,'MEDRE'
+                        ,'MIX'
+                    )
+                        AND AGRUPACION = 'PROMOS'
+                        AND UEN = 'PERDRIEL'
+                        AND CAST(FECHASQL as date) > @dosSemanasAtras
+                    AND CAST(FECHASQL as date) <= @semanaAtras
+                    GROUP BY UEN)
+                ) as t
+
+            group by t.UEN
+            ) as GrillAnterior
+        ) as GrillAnterior
+            ON GrillActual.UEN = GrillAnterior.UEN
+        """
+        , conexCentral
+    )
+
+    # print(df_grill.info(), df_grill)
+
+    ##########################################################
+    # Adding Total Row and "Var %" columns
+    ##########################################################
+
+    # Creating Total Row
+    _temp_tot = df_grill.drop(columns=["UEN"]).sum()
+    _temp_tot["UEN"] = "TOTAL"
+
+    # Appending Total Row
+    df_grill = df_grill.append(_temp_tot, ignore_index=True)
+
+    # Create columns "Var % Cantidad"
+    df_grill["Var % Cantidad"] = \
+        df_grill["Unid Vend Sem Actual"] / df_grill["Unid Vend Sem Ant"] -1
+
+    # Create columns "Var % Importe"
+    df_grill["Var % Importe"] = (
+        df_grill["Importe Total Sem Actual"] 
+        / df_grill["Importe Total Sem Ant"] 
+        -1
+    )
+
+    # print(df_grill)
+
+    return df_grill
+
+
+
+##########################################
+# STYLING of the dataframe
+##########################################
+
+
+def _estiladorVtaTitulo(
+    df:pd.DataFrame
+    , list_Col_Num=[]
+    , list_Col_Din=[]
+    , list_Col_Perc=[]
+    , titulo=""
+):
+    """
+This function will return a styled dataframe that must be assign to a variable.
+ARGS:
+    df: Dataframe that will be styled.
+    list_Col_Num: List of numeric columns that will be formatted with
+    zero decimals and thousand separator.
+    list_Col_Din: List of numeric columns that will be formatted with money 
+    symbol, zero decimals and thousand separator.
+    list_Col_Perc: List of numeric columns that will be formatted 
+    as percentage.
+    titulo: String for the table caption.
+    """
+    resultado = df.style \
+        .format("{0:,.0f}", subset=list_Col_Num) \
+        .format("$ {0:,.0f}", subset=list_Col_Din) \
+        .format("{:,.2%}", subset=list_Col_Perc) \
+        .hide_index() \
+        .set_caption(
+            titulo
+            + "<br>"
+            + "Semana Actual "
+            + ((pd.to_datetime("today")-pd.to_timedelta(7,"days"))
+            .strftime("%d/%m/%y"))
+            + " al "
+            + ((pd.to_datetime("today")-pd.to_timedelta(1,"days"))
+            .strftime("%d/%m/%y"))
+        ) \
+        .set_properties(subset=list_Col_Num + list_Col_Din + list_Col_Perc
+            , **{"text-align": "center", "width": "100px"}) \
+        .set_properties(border= "2px solid black") \
+        .set_table_styles([
+            {"selector": "caption", 
+                "props": [
+                    ("font-size", "20px")
+                    ,("text-align", "center")
+                ]
+            }
+            , {"selector": "th", 
+                "props": [
+                    ("text-align", "center")
+                    ,("background-color","black")
+                    ,("color","white")
+                    ,("font-size", "14px")
+                ]
+            }
+        ]) \
+        .apply(lambda x: ["background-color: black" if x.name == df.index[-1] 
+            else "" for i in x]
+            , axis=1) \
+        .apply(lambda x: ["color: white" if x.name == df.index[-1]
+            else "" for i in x]
+            , axis=1) \
+        .apply(lambda x: ["font-size: 15px" if x.name == df.index[-1]
+            else "" for i in x]
+            , axis=1)
+
+    return resultado
+
+
+
+##########################################
+# PRINTING dataframe as an image
+##########################################
+
+# This will print the df with a unique name and will erase the old image 
+# everytime the script is run
+
+def _df_to_image(df, ubicacion, nombre):
+    """
+    Esta función usa las biblioteca "dataframe_Image as dfi" y "os" para 
+    generar un archivo .png de un dataframe. Si el archivo ya existe, este será
+    reemplazado por el nuevo archivo.
+
+    Args:
+        df: dataframe a convertir
+        ubicacion: ubicacion local donde se quiere grabar el archivo
+         nombre: nombre del archivo incluyendo extensión .png (ej: "hello.png")
+
+    """
+        
+    if os.path.exists(ubicacion+nombre):
+        os.remove(ubicacion+nombre)
+        dfi.export(df, ubicacion+nombre)
+    else:
+        dfi.export(df, ubicacion+nombre)
+
+
+
+##########################################
+# MERGING images
+##########################################
+
+def _append_images(listOfImages, direction='horizontal',
+                  bg_color=(255,255,255), alignment='center'):
+    """
+    Appends images in horizontal/vertical direction.
+
+    Args:
+        listOfImages: List of images with complete path
+        direction: direction of concatenation, 'horizontal' or 'vertical'
+        bg_color: Background color (default: white)
+        alignment: alignment mode if images need padding;
+           'left', 'right', 'top', 'bottom', or 'center'
+
+    Returns:
+        Concatenated image as a new PIL image object.
+    """
+    images = [Image.open(x) for x in listOfImages]
+    widths, heights = zip(*(i.size for i in images))
+
+    if direction=='horizontal':
+        new_width = sum(widths)
+        new_height = max(heights)
+    else:
+        new_width = max(widths)
+        new_height = sum(heights)
+
+    new_im = Image.new('RGB', (new_width, new_height), color=bg_color)
+
+    offset = 0
+    for im in images:
+        if direction=='horizontal':
+            y = 0
+            if alignment == 'center':
+                y = int((new_height - im.size[1])/2)
+            elif alignment == 'bottom':
+                y = new_height - im.size[1]
+            new_im.paste(im, (offset, y))
+            offset += im.size[0]
+        else:
+            x = 0
+            if alignment == 'center':
+                x = int((new_width - im.size[0])/2)
+            elif alignment == 'right':
+                x = new_width - im.size[0]
+            new_im.paste(im, (x, offset))
+            offset += im.size[1]
+
+    return new_im
+
+
+
+##########################################
+# FUNCTION TO RUN MODULE
+##########################################
+
+def perifericoSemanal():
+    """
+    
+    """
+
+    # Timer
+    tiempoInicio = pd.to_datetime("today")
+
+    # Connection to DBs
+    conexCentral = conectorMSSQL(login)
+    conexAZMil = conectorMSSQL(loginAZMilenium)
+    conexPIMil = conectorMSSQL(loginPIMilenium)
+
+    # Getting DFs
+    df_SCFull = _get_SCFull(conexCentral, conexAZMil, conexPIMil)
+    df_pan = _get_pan(conexCentral, conexAZMil, conexPIMil)
+    df_lubriplaya = _get_lubriplaya(conexCentral)
+    df_grill = _get_grill(conexCentral)
+
+    # Styling of DFs
+    df_SCFull_Estilo = _estiladorVtaTitulo(
+        df=df_SCFull
+        , list_Col_Num=[
+            "Unid Vend Sem Ant"
+            , "Unid Vend Sem Actual"
+        ]
+        , list_Col_Din=[
+            "Importe Total Sem Ant"
+            , "Importe Total Sem Actual"
+        ]
+        , list_Col_Perc=[
+            "Var % Cantidad"
+            , "Var % Importe"
+        ]
+        , titulo="Servicompras y Fulls"
+    )
+
+    df_pan_Estilo = _estiladorVtaTitulo(
+        df=df_pan
+        , list_Col_Num=[
+            "Doc Vend Sem Ant"
+            , "Doc Vend Sem Actual"
+        ]
+        , list_Col_Din=[
+            "Importe Total Sem Ant"
+            , "Importe Total Sem Actual"
+        ]
+        , list_Col_Perc=[
+            "Var % Cantidad"
+            , "Var % Importe"
+        ]
+        , titulo="Panificados"
+    )
+
+    df_lubriplaya_Estilo = _estiladorVtaTitulo(
+        df=df_lubriplaya
+        , list_Col_Num=[
+            "Unid Vend Sem Ant"
+            , "Unid Vend Sem Actual"
+        ]
+        , list_Col_Din=[
+            "Importe Total Sem Ant"
+            , "Importe Total Sem Actual"
+        ]
+        , list_Col_Perc=[
+            "Var % Cantidad"
+            , "Var % Importe"
+        ]
+        , titulo="Lubriplaya"
+    )
+
+    df_grill_Estilo = _estiladorVtaTitulo(
+        df=df_grill
+        , list_Col_Num=[
+            "Unid Vend Sem Ant"
+            , "Unid Vend Sem Actual"
+        ]
+        , list_Col_Din=[
+            "Importe Total Sem Ant"
+            , "Importe Total Sem Actual"
+        ]
+        , list_Col_Perc=[
+            "Var % Cantidad"
+            , "Var % Importe"
+        ]
+        , titulo="Grill"
+    )
+
+
+    # Files location
+    ubicacion = str(pathlib.Path(__file__).parent)+"\\"
+
+    # Printing Images
+    _df_to_image(df_SCFull_Estilo, ubicacion, "SCFull_Sem.png")
+    _df_to_image(df_pan_Estilo, ubicacion, "Pan_Sem.png")
+    _df_to_image(df_lubriplaya_Estilo, ubicacion, "Lubriplaya_Sem.png")
+    _df_to_image(df_grill_Estilo, ubicacion, "Grill_Sem.png")
+
+    # Merge images vertically
+    listaImg1 = [
+        ubicacion + "SCFull_Sem.png"
+        , ubicacion + "Pan_Sem.png"
+    ]
+
+    listaImg2 = [
+        ubicacion + "Lubriplaya_Sem.png"
+        , ubicacion + "Grill_Sem.png"
+    ]
+
+
+    fusionImg = _append_images(listaImg1, direction="vertical")
+    fusionImg.save(ubicacion + "Info_Periferia_parte1.png")
+
+    fusionImg2 = _append_images(listaImg2, direction="vertical")
+    fusionImg2.save(ubicacion + "Info_Periferia_parte2.png")
+
+
+    # Timer
+    tiempoFinal = pd.to_datetime("today")
+    logger.info(
+        "Info Periferia Semanal"
+        + "\nTiempo de Ejecucion Total: "
+        + str(tiempoFinal-tiempoInicio)
+    )
+
+
+
+if __name__ == "__main__":
+    perifericoSemanal()
